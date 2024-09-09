bbb49f8cb34a25036f5e015df9683b501239b248
Sagar Khalasi <tester.sagar.007@gmail.com>
2024-09-09 22:59:26 +0530

    Update FailingCase_spec.ts

--- app/client/cypress/e2e/Regression/ClientSide/ActionExecution/FailingCase_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/ActionExecution/FailingCase_spec.ts
@@ -1,16 +1,5 @@
-import {
-  agHelper,
-  deployMode,
-  entityExplorer,
-  jsEditor,
-  propPane,
-} from "../../../../support/Objects/ObjectsCore";
-import EditorNavigation, {
-  EntityType,
-} from "../../../../support/Pages/EditorNavigation";
-
-describe("clearStore Action test", { tags: ["@tag.JS"] }, () => {
+describe("clearStore Action test", () => {
   it("1. Feature 11639 : Clear all store value", function () {
-    expect(true).to.be.equals(true);
+    expect(true).to.be.equals(false);  // This will make the test case fail
   });
 });
