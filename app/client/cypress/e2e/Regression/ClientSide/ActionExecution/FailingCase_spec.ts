6177da7253629b0acb8da367e7c1a7a844d367e1
Sagar Khalasi <sagar@appsmith.com>
2024-09-09 22:47:08 +0530

    failing test case

---
+++ app/client/cypress/e2e/Regression/ClientSide/ActionExecution/FailingCase_spec.ts
@@ -0,0 +1,16 @@
+import {
+  agHelper,
+  deployMode,
+  entityExplorer,
+  jsEditor,
+  propPane,
+} from "../../../../support/Objects/ObjectsCore";
+import EditorNavigation, {
+  EntityType,
+} from "../../../../support/Pages/EditorNavigation";
+
+describe("clearStore Action test", { tags: ["@tag.JS"] }, () => {
+  it("1. Feature 11639 : Clear all store value", function () {
+    expect(true).to.be.equals(true);
+  });
+});
