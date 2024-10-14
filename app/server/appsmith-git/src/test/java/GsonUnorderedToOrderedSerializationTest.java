9097ddc4b4e9d502edb4b389a04a7fa293a500b3
Sagar Khalasi <sagar@appsmith.com>
2024-10-14 17:23:16 +0530

    small update in server

--- app/server/appsmith-git/src/test/java/GsonUnorderedToOrderedSerializationTest.java
+++ app/server/appsmith-git/src/test/java/GsonUnorderedToOrderedSerializationTest.java
@@ -24,6 +24,7 @@ public class GsonUnorderedToOrderedSerializationTest {
     @Test
     public void convert_whenEmptySet_returnsEmptyArrayString() {
         Set data = new HashSet();
+        System.out.println(data);
         String orderedData = gson.toJson(data);
         assertThat(orderedData).isEqualTo("[]");
     }
